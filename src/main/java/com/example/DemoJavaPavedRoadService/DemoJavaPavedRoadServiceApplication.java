73d7fbdbf6cf2e28cdf16d450c5aa339f241fc1d
muthuraj <muthu_raj27@yahoo.co.in>
2024-06-04 15:21:46 +0530

    initial

---
+++ src/main/java/com/example/DemoJavaPavedRoadService/DemoJavaPavedRoadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoJavaPavedRoadService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaPavedRoadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaPavedRoadServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DemoJavaPavedRoadService/rest/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.DemoJavaPavedRoadService.rest;
+
+public record Greeting(long id, String content) { }
